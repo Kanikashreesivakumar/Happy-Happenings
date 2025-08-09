d1054a9dff042acef979e26e5bf1078fbb274fcd
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-08-09 17:50:37 +0530

    added

--- app/book-now/page.tsx
+++ app/book-now/page.tsx
@@ -403,7 +403,7 @@ export default function BookNowPage() {
                             <SelectContent>
                               <SelectItem value="under-5k">Under rupees 5,000</SelectItem>
                               <SelectItem value="5k-10k">rupees 5,000 - 10,000</SelectItem>
-                              <SelectItem value="10k-20k">10,000 - 20,000</SelectItem>
+                              <SelectItem value="10k-20k">ruppees  20,000</SelectItem>
                               <SelectItem value="20k-50k">20,000 - 50,000</SelectItem>
                               <SelectItem value="50k+">50,000+</SelectItem>
                             </SelectContent>
