a9e89d31451a35dd6a50101aa09856283a9e83e9
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2025-08-10 18:12:20 +0530

    Initial commit

--- app/book-now/page.tsx
+++ app/book-now/page.tsx
@@ -460,7 +460,7 @@ export default function BookNowPage() {
                             >
                               {pkg.popular && (
                                 <Badge className="absolute -top-2 left-1/2 transform -translate-x-1/2 bg-rose-500 text-white">
-                                  Most Popular
+                                  Most popular
                                 </Badge>
                               )}
                               <div className="text-center">
